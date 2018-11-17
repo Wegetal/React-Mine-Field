d9afe343144ffb7136c8222ea11ff783ed2ef8f0
Davi Wegner <daviwegner@gmail.com>
2018-11-17 13:26:11 -0200

    added tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-
-it('renders without crashing', () => {
-  const div = document.createElement('div');
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import Board from "./components/Board";
+import Cell from "./components/Cell";
+it("renders without crashing", () => {
+  const div = document.createElement("div");
   ReactDOM.render(<App />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
+it("renders without crashing", () => {
+  const div = document.createElement("div");
+  ReactDOM.render(<Board />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
+it("renders without crashing", () => {
+  const div = document.createElement("div");
+  ReactDOM.render(<Cell />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
